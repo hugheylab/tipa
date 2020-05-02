0ef415c3649213cb1ae48fab241b8c3ac7fc023a
Jake Hughey <jakejhughey@gmail.com>
2020-05-02 18:55:39 -0500

    cleaned up documentation.

--- R/tipa_example_cosinor.R
+++ R/tipa_example_cosinor.R
@@ -1,12 +1,12 @@
 # Time-course data from multiple (simulated) experiments
-getTimecourseFile = function() system.file('extdata', 'timecourses.csv', package = 'tipa')
+getTimecourseFile = function() {
+  system.file('extdata', 'timecourses.csv', package = 'tipa')}
 df = read.csv(getTimecourseFile(), stringsAsFactors = FALSE)
 
 resultList = lapply(sort(unique(df$expId)), function(ii) {
-	time = df$time[df$expId == ii]
-	y = df$intensity[df$expId == ii]
-   tipaCosinor(time, y, stimOnset = 0)
-})
+  time = df$time[df$expId == ii]
+  y = df$intensity[df$expId == ii]
+  tipaCosinor(time, y, stimOnset = 0)})
 
 phaseShifts = sapply(resultList, function(r) r$phaseShift)
 
